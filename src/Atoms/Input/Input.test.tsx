82a52712d5d842bf95a997590a1fb1777302bf6f
Tommy Scribble <tommy@scribbledesign.co.uk>
2020-10-31 17:20:12 +0000

    update tests around Inputs

--- src/Atoms/Input/Input.test.tsx
+++ src/Atoms/Input/Input.test.tsx
@@ -1,8 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render, getByText, fireEvent } from '@testing-library/react';
 import Input from './index';
+
+const label = 'This is a label';
+const userText = 'Some user text';
+const id = 'test-input';
+const placeholder = 'Enter some text';
+
+const setup = (type: 'text' | 'textarea' | 'checkbox') => {
+	const container = render(
+		<Input type={type} label={label} id={id} placeholder={placeholder} />
+	);
+	const input = container.getByLabelText(label) as HTMLInputElement;
+	return {
+		input,
+		...container,
+	};
+};
 
 it('renders without crashing', () => {
 	const div = document.createElement('div');
-	ReactDOM.render(<Input type='text' />, div);
+	ReactDOM.render(<Input type="text" />, div);
+});
+
+it('displays a text input with label and allows user to enter text', () => {
+	const { input } = setup('text');
+	expect(input.value).toBe('');
+	fireEvent.change(input, { target: { value: userText } });
+	expect(input.value).toBe(userText);
+});
+
+it('displays custom placeholder in textarea input', () => {
+	const { getByPlaceholderText } = setup('textarea');
+	expect(getByPlaceholderText(placeholder)).toBeInTheDocument();
+});
+
+it('displays a checkbox input and toggles when clicked', () => {
+	const { getByLabelText } = setup('checkbox');
+	const checkbox = getByLabelText(label) as HTMLInputElement;
+	expect(checkbox.checked).toEqual(false);
+	fireEvent.click(checkbox);
+	expect(checkbox.checked).toEqual(true);
+	fireEvent.click(checkbox);
+	expect(checkbox.checked).toEqual(false);
 });
