458bf83a98b35abf857e9d8e3db39b2a08d67963
Tommy Scribble <tommy@scribbledesign.co.uk>
2020-10-31 15:43:40 +0000

    add more tests around Button

--- src/Atoms/Button/Button.test.tsx
+++ src/Atoms/Button/Button.test.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { fireEvent, render } from '@testing-library/react';
+
 import Button from './index';
 
-const buttonText = 'Test text'
-const btnClassType = 'primary'
+let buttonText = 'Test text';
+const btnClassType = 'primary';
 
 it('renders without crashing', () => {
 	const div = document.createElement('div');
 	ReactDOM.render(<Button btnClass={btnClassType}>{buttonText}</Button>, div);
+});
+
+it('displays the correct text to the user', () => {
+	const container = render(
+		<Button btnClass={btnClassType}>{buttonText}</Button>
+	);
+	expect(container.getByText(buttonText)).toBeInTheDocument();
+});
+
+it('fire onClick event when clicked', () => {
+	const clickEvent = jest.fn();
+	const { getByText } = render(
+		<Button btnClass={btnClassType} onClick={clickEvent}>
+			{buttonText}
+		</Button>
+	);
+	const button = getByText(buttonText);
+
+	fireEvent.click(button);
+	expect(clickEvent).toHaveBeenCalled();
 });
