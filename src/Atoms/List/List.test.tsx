cef91d6b871a43e6fd770041ce7333d03cbd1a52
Tommy Scribble <tommy@scribbledesign.co.uk>
2020-10-31 17:31:24 +0000

    add more tests around lists

--- src/Atoms/List/List.test.tsx
+++ src/Atoms/List/List.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import List from './index';
 
 const items = ['item 01', 'item 02', 'item 03'];
@@ -7,4 +8,20 @@ const items = ['item 01', 'item 02', 'item 03'];
 it('renders without crashing', () => {
 	const div = document.createElement('div');
 	ReactDOM.render(<List listItems={items} />, div);
+});
+
+it('displays an unordered list with 3 items', () => {
+	const { container } = render(<List listItems={items} />);
+	const parentElement = container.querySelector('ul');
+	expect(parentElement).toBeInTheDocument();
+	const listItems = container.querySelectorAll('li');
+	expect(listItems.length).toBe(3);
+});
+
+it('displays an ordered list with 3 items', () => {
+	const { container } = render(<List listItems={items} ordered={true} />);
+	const parentElement = container.querySelector('ol');
+	expect(parentElement).toBeInTheDocument();
+	const listItems = container.querySelectorAll('li');
+	expect(listItems.length).toBe(3);
 });
