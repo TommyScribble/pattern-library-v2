961d23e15019e4aeaa8a2904f09ae4ebe2acc886
Tommy Scribble <tommy@scribbledesign.co.uk>
2021-03-11 07:54:39 +0000

    remove margins from example headings

--- src/05-Pages/atoms/headings/index.tsx
+++ src/05-Pages/atoms/headings/index.tsx
@@ -3,8 +3,15 @@ import React from 'react';
 import content from '../../../PageData/HeadingspageData.json';
 import ComponentPage from '../../../04-Templates/ComponentPage';
 
+import style from './headings.module.scss';
+
 const buttons = () => {
-	return <ComponentPage content={content} />;
+	return (
+		<ComponentPage
+			content={content}
+			accordionSectionClass={style.headings}
+		/>
+	);
 };
 
 export default buttons;
