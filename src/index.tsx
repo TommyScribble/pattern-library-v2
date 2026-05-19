21f5c128b25b6cd93da4a99c7ef6b6ba7048c23c
Tommy <tommy@scribbledesign.co.uk>
2026-05-19 08:37:53 +0100

    upgrade node and start updating dependancies

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import '../src/styles/global.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+	createRoot(rootElement).render(
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	);
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
